1a2c6f916d3ef7427ec47cfe0fff3f23e5a5112c
RangerRhino23 <ryadahlke@outlook.com>
2023-07-09 15:25:24 -0500

    new code

---
+++ level1.js
@@ -0,0 +1,14 @@
+//Direction Changers Here
+DirectionChanger(x=-.3,y=.1,direcitonChange='Down')
+DirectionChanger(x=.3,y=.1,direcitonChange='Right')
+DirectionChanger(x=.3,y=.0,direcitonChange='Up')
+
+//End Points Here
+EndPoint(x=-.3,y=-.2,blockcolor=color.blue)
+EndPoint(x=.5,y=.1,blockcolor=color.red)
+EndPoint(x=-.5,y=.1,blockcolor=color.orange)
+
+//Squares Here
+Square(blockcolor=color.blue,x=.1,y=.1,direction='Left')
+Square(blockcolor=color.red,x=-.1,y=0,direction='Right')
+Square(blockcolor=color.orange,x=-.3,y=0,direction='Right')
